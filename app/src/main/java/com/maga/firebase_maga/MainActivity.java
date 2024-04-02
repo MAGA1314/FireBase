367fb946f3c008315a78b34dc29672b1c8934896
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-04-02 08:30:55 -0500

    archivos

---
+++ app/src/main/java/com/maga/firebase_maga/MainActivity.java
@@ -0,0 +1,14 @@
+package com.maga.firebase_maga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
